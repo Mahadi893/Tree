9a5556cabd08dc9aaac40c3441a5541060d23668
Mahadi893 <mahadirefat100@gmail.com>
2025-12-31 11:24:24 -0800

    Implement binary tree height calculation

---
+++ Height.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+static int idx = -1;
+Node* buildTree(vector<int>preorder)
+{
+    idx++;
+    if(preorder[idx] == -1)
+    {
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+    return root;
+}
+void postorderPrint(Node* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    postorderPrint(root->left);
+    postorderPrint(root->right);    
+    cout<<root->data<<" ";
+}
+int height(Node* root)
+{
+    if(root == NULL)
+    {
+        return 0;
+    }
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    return max(leftHeight,rightHeight) + 1;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<int>preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root = buildTree(preorder);
+    postorderPrint(root);
+    cout<<endl;
+    cout<<"Height of tree : "<<height(root)<<endl;
+
+    return 0;
+}
+
+// postorder : left right root
